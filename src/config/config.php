1c69d28a7590b2fd318ad52b23dd691c00ec3d3c
debuqer <bagher2g@gmail.com>
2021-09-18 21:46:12 +0430

    add log-model-changes to config

--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,7 @@
 
 use Escherchia\LaravelScenarioLogger\Logger\Services\LogException;
 use Escherchia\LaravelScenarioLogger\Logger\Services\LogManualTrace;
+use Escherchia\LaravelScenarioLogger\Logger\Services\LogModelChanges;
 use Escherchia\LaravelScenarioLogger\Logger\Services\LogRequest;
 use Escherchia\LaravelScenarioLogger\Logger\Services\LogResponse;
 
@@ -33,6 +34,10 @@ return [
         'log-manual-trace' => [
             'active' => true,
             'class' => LogManualTrace::class,
+        ],
+        'log-model-changes' => [
+            'active' => true,
+            'class' => LogModelChanges::class,
         ],
     ],
 ];
